527564f762edfbd68af489306d8101129b448cb9
Josh Lim <email": null>
2017-06-07 14:11:12 -0700

    Links in Trends

--- app/assets/javascripts/hashtags_controller.js
+++ app/assets/javascripts/hashtags_controller.js
@@ -7,14 +7,23 @@ $(document).ready(function() {
   }).done(function(response) {
     trending.html("")
     response.forEach(function(tag) {
-      var span = "<span style='color: grey'> &#128038; " + tag.hashtag_count + "</span>"
+      var span = "<span style='color: grey'> &#128038; " + tag.hashtag_count + "| </span>"
       var search_path = '/tweets/search/' + tag.name
-      var link = "<a href='" + search_path + "'>" + tag.name + "</a>"
-      trending.prepend("<li>" + link + span + "</li>")
+      var link = "<a class='trend' href='" + search_path + "'>" + tag.name + "</a>"
+      trending.append("<li>" + span + link + "</li>")
     })
   })
 
-
+  $("body").on("click", "a.trend", function(e){
+    e.preventDefault()
+    var that = $(this)
+    $.ajax({
+      method: "GET",
+      url: "tweets/search/" + that.text()
+    }).done(function(response) {
+      console.log(response)
+    })
+  })
 
 
 })
