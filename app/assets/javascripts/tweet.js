574bfa7d15f30c6db6b1575e8974d9d56ee909d6
Jared Graham & Paul Vander <pair+jaredgraham12.paulsvander@devbootcamp.com>
2017-06-07 11:05:02 -0700

    Remove console.logs used for testing

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -3,7 +3,6 @@ function fetchRecent(){
     url: 'http://localhost:3000/tweets/recent',
     dataType: 'json'
   })
-  console.log('recent tweets in model are ' + recentTweets)
   return recentTweets;
 }
 
--- app/assets/javascripts/tweet_controller.js
+++ app/assets/javascripts/tweet_controller.js
@@ -5,8 +5,6 @@ $(document).ready(function(){
 function handleRecentTweets() {
   var promiseFromAjax = fetchRecent();
   promiseFromAjax.done(function(recentData){
-    console.log(new Date(recentData[0].updated_at))
-    console.log(Math.floor(((new Date() - new Date(recentData[0].updated_at))/1000)/60))
     showRecent(recentData)
   })
 }
