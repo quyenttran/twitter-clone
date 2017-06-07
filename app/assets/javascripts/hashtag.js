fb087fa7775d7ee94717e3d2c74c8105440e1072
Erik Bonn & Ian Gonnella & Lisa D'Asaro & Mendel Dinh <pair+erikbonn.iagon.lmdasaro.mdinh7@devbootcamp.com>
2017-06-07 12:27:41 -0700

    finished milestone 5

--- app/assets/javascripts/hashtag.js
+++ app/assets/javascripts/hashtag.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-  // var hashTagger = function(json){
-  //   json.forEach(function(hashTag){
-  //     thi
-  //   })
-  //
-  //
-  // }
-
   $.ajax({
     method: "GET",
     url: "/hashtags/popular",
@@ -19,4 +11,36 @@ $(document).ready(function(){
       $("#trends-container").find("li:nth-child(" + j + ")").text(response[i].name)
     }
   })
+
+  $("#trends-container").on("click", "li", function(e){
+    e.preventDefault()
+    var hashTag = $(this).text()
+    console.log(hashTag)
+
+    $.ajax({
+      method: "GET",
+      url: "/tweets/search/" + hashTag,
+      data: { name: hashTag},
+      dataType: "json"
+    })
+
+    .done(function(response){
+      for(var i = 0; i < response.length; i++){
+      var j = (i + 1)
+      var createdAt = (new Date(response[i].created_at))
+      var currentTime = Math.floor((Date.now() - (createdAt)) / 1000 / 60)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(currentTime + " min")
+
+      $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
+    }
+  })
+
+
+  })
+
+
+
 })
