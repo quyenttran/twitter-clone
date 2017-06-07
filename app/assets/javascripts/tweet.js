023583bf1e330ef8287fcee43dd7065678438bf4
Erik Bonn & Ian Gonnella & Lisa D'Asaro & Mendel Dinh <pair+erikbonn.iagon.lmdasaro.mdinh7@devbootcamp.com>
2017-06-07 11:20:25 -0700

    current working master

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -17,9 +17,14 @@ $(document).ready(function(){
     .done(function(response){
       for(var i = 0; i < response.length; i++){
       var j = (i + 1)
+      var currentTime = ((Date(Date.now()) - response[i].created_at) / 1000 / 60)
+      console.log(currentTime)
       $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
-      console.log($("#tweets-container").find("li:nth-child(" + j + ")").find("img"))
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(response[i].currentTime)
+
       $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
     }
   })
 })
