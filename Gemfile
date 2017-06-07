368c6cd58d747133f1d0204317be73abaffae856
Lisa D'Asaro <lmdasaro@gmail.com>
2017-06-07 15:35:07 -0700

    worked on search bar functionality

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ end
 
 
 group :development, :test do
-  gem 'rspec-rails', '~>2.14'
+  gem 'rspec-rails', '~> 3.0'
   gem 'factory_girl_rails'
   gem 'faker'
   gem 'jasmine-rails'
--- app/assets/javascripts/hashtag.js
+++ app/assets/javascripts/hashtag.js
@@ -19,4 +19,34 @@ $(document).ready(function(){
       $("#trends-container").find("li:nth-child(" + j + ")").text(response[i].name)
     }
   })
+$("#search-form").on("focusout", function(e){
+  e.preventDefault()
+  console.log("hello??")
+  var keyword = $("#search").val()
+  console.log(keyword)
+
+  $.ajax({
+    method: "GET",
+    url: "/tweets/search/" + keyword,
+    dataType: "json"
+  })
+  .done(function(response){
+    console.log(response.status)
+    for(var i = 0; i < response.length; i++){
+      var j = (i + 1)
+      var createdAt = (new Date(response[i].created_at))
+      var currentTime = Math.floor((Date.now() - (createdAt)) / 1000 / 60)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(currentTime + " min")
+
+      $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
+      }
+    })
+    .fail(function(response){
+      console.log("FAIL")
+      $("#search").css("background-color", "red")
+    })
+  })
 })
