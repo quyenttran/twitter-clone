892287e9452332fefb50ea42799eaacb1d0006a6
Quyen Tran <email": null>
2017-06-07 10:38:55 -0700

    Successfully start app with Ember

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ TwitterDashboardApi::Application.routes.draw do
   post 'tweets', to: 'tweets#create'
   get 'hashtags/popular', to: 'hashtags#popular'
 
-  root to: 'static#index'
+  # root to: 'static#index'
+  mount_ember_app :frontend, to: "/"
 end
