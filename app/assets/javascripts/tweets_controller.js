7a7f5711495fe48407f7c29bc393963e41731553
Quyen Tran <email": null>
2017-06-07 16:44:08 -0700

    Add search functions

--- app/assets/javascripts/tweets_controller.js
+++ app/assets/javascripts/tweets_controller.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   fetchRecentTweets();
+
   $("#tweet-form").submit(function(event) {
     event.preventDefault();
     var tweetContent = $("#new-tweet").val();
@@ -9,11 +10,43 @@ $(document).ready(function() {
       url: "/tweets",
       data: { tweet: {content: tweetContent} , hashtags: tweetHashtags }
     }).done(function(response){
-      console.log(response)
       $("#tweets-container").find("ul").prepend(renderTweet(response));
     })
   })
+
+  $("#search-form").keypress(function(event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      event.preventDefault();
+      var searchTags = $("#search").val();
+      var promise = searchedTweets(searchTags);
+      promise.fail(function(response){
+        $("#search").css("background-color", "pink");
+      })
+      promise.done(function(response) {
+        showSearchedTweets(searchTags);
+      })
+    }
+  })
 })
+
+function showSearchedTweets(hashtag) {
+  var promiseFromAjax = searchedTweets(hashtag);
+  promiseFromAjax.done(function(response) {
+    response.forEach(function(tweet) {
+      $("#tweets-container").find("ul").children().remove();
+      $("#tweets-container").find("ul").append(renderTweet(tweet));
+    })
+  })
+}
+
+function searchedTweets(hashtag) {
+  var requestPromise = $.ajax({
+    url: "/tweets/search/" + hashtag,
+    method: "GET"
+  })
+  return requestPromise;
+}
 
 function findHashtags(tweet) {
   var hashtags = []
