0a49398a0d1f0022f2001959de7b821b707fa468
Josh Lim & Paul Newsam & Ryan Austin <pair+joshhlim.monkypuzzle.ryana760@devbootcamp.com>
2017-06-07 12:00:48 -0700

    Fixes prepend new tweet to tweet river function

--- app/assets/javascripts/tweets_controller.js
+++ app/assets/javascripts/tweets_controller.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-  var tweetForm = $("#tweet-form")
-  var tweetRiver = $("#tweets-container").find("ul")
+  var $tweetForm = $("#tweet-form")
+  var $tweetRiver = $("#tweets-container").find("ul")
+  var $tweetFormBody = $('#new-tweet')
 
-// Create Views object
+// Create TweetsViews object
   var tweetsViews = new TweetsViews
 
 // POST new tweet data to server to create tweet
-  tweetForm.on("submit", function(event){
+  $tweetForm.on("submit", function(event){
     event.preventDefault();
     var $that = $(this);
-    var tweet = new Tweet({content: $('#new-tweet').val()});
+    var tweet = new Tweet({content: $tweetFormBody.val()});
     var data = {tweet: {hashtag_names: tweet.hashtag_names, content: tweet.content}}
     $.ajax({
       method: 'post',
@@ -19,21 +20,22 @@ $(document).ready(function(){
     })
     // Prepend response to Tweet River
     .done(function(response){
-      // console.log(response)
-      tweetRiver.prepend(response)
+      $tweetRiver.prepend(tweetsViews.renderTweet(response))
     });
   });
 
-// GET most recent 50 tweets and add them to the tweets container
+// GET most recent 50 tweets
   $.ajax({
     method: 'get',
     url: '/tweets/recent'
   })
   .done(function(response){
     tweets = ""
+    // Erase current content of $tweetRiver
+    $tweetRiver.html("")
+    // Prepend each tweet
     response.forEach(function(tweet){
-    // console.log(tweet)
-      tweetRiver.prepend(tweetsViews.renderTweet(tweet));
+      $tweetRiver.append(tweetsViews.renderTweet(tweet));
     })
   });
 
