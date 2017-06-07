112837faad4a71207be0d4003b2883b19365a270
Erik Bonn & Ian Gonnella & Lisa D'Asaro & Mendel Dinh <pair+erikbonn.iagon.lmdasaro.mdinh7@devbootcamp.com>
2017-06-07 11:40:17 -0700

    finished timing and 1st milestone

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-  // var hashTagger = function(json){
-  //   json.forEach(function(hashTag){
-  //     thi
-  //   })
-  //
-  //
-  // }
-
   $.ajax({
     method: "GET",
     url: "/tweets/recent",
@@ -17,11 +9,11 @@ $(document).ready(function(){
     .done(function(response){
       for(var i = 0; i < response.length; i++){
       var j = (i + 1)
-      var currentTime = ((Date(Date.now()) - response[i].created_at) / 1000 / 60)
-      console.log(currentTime)
+      var createdAt = (new Date(response[i].created_at))
+      var currentTime = Math.floor((Date.now() - (createdAt)) / 1000 / 60)
       $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
       $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(response[i].currentTime)
+      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(currentTime + " min")
 
       $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
       $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
