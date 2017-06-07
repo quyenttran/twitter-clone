9aa49ab4fdbf5b8d8d92da22ab9ef3c164dad8e7
Quyen Tran <email": null>
2017-06-07 13:41:40 -0700

    Show 50 recent tweets in river

--- app/assets/javascripts/tweets_controller.js
+++ app/assets/javascripts/tweets_controller.js
@@ -1,11 +1,18 @@
-function fetchRecentTweets() {
-  $.get("http://localhost:3000/tweets/recent", function(data) {
-    recentTweets = data;
-  })
-  return recentTweets;
-}
+  function renderTweet(tweet) {
+    return "<li class='tweet'><img class='avatar' src='" + tweet.avatar_url + "' alt=''><div class='tweet-content'><p><span class='full-name'>" + tweet.username + "</span><span class='username'>" + tweet.handle + "</span><span class='timestamp'>- " + tweet.created_at + "</span></p><p>" + tweet.content + "</p></div></li>"
+  }
+  function fetchRecentTweets() {
+    var recentTweets;
+    $.get("http://localhost:3000/tweets/recent", function(data) {
+      recentTweets = data;
+      recentTweets.forEach(function(tweet){
+        $("#tweets-container").append(renderTweet(tweet));
+      })
+    })
+  }
 
-function renderTweet(tweet) {
-  return "<li class='tweet'><img class='avatar' src='" + tweet.avatar_url + "' alt=''><div class='tweet-content'><p><span class='full-name'>" + tweet.username + "</span><span class='username'>" + tweet.handle + "</span><span class='timestamp'>- " + tweet.created_at + "</span></p><p>" + tweet.content + "</p></div></li>"
-}
+$(document).ready(function() {
+  fetchRecentTweets();
+})
+
 
