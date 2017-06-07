a10739a8ad96b390364ce5c1a3b3bdb0338fb8cc
Erik Bonn & Ian Gonnella & Lisa D'Asaro & Mendel Dinh <pair+erikbonn.iagon.lmdasaro.mdinh7@devbootcamp.com>
2017-06-07 16:47:58 -0700

    searches on pressing enter

--- app/assets/javascripts/hashtag.js
+++ app/assets/javascripts/hashtag.js
@@ -1,76 +1,71 @@
-$(document).ready(function(){
+var riverUpdater = function(response){
+  for(var i = 0; i < response.length; i++){
+  var j = (i + 1)
+  var createdAt = (new Date(response[i].created_at))
+  var currentTime = Math.floor((Date.now() - (createdAt)) / 1000 / 60)
+  $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
+  $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
+  $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(currentTime + " min")
 
-  $.ajax({
-    method: "GET",
-    url: "/hashtags/popular",
-    dataType: "json"
-  })
-    .done(function(response){
-      for(var i = 0; i < response.length; i++){
-      var j = (i + 1)
-      $("#trends-container").find("li:nth-child(" + j + ")").text(response[i].name)
-    }
-  })
+  $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
+  $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
+  $("#search").val("")
+ }
+}
 
-$("#search-form").on("focusout", function(e){
-  e.preventDefault()
-  console.log("hello??")
-  var keyword = $("#search").val()
-  console.log(keyword)
+var popularHashtagsLoadHandler = function(response){
+  for(var i = 0; i < response.length; i++) {
+    var j = (i + 1)
+    $("#trends-container").find("li:nth-child(" + j + ")").text(response[i].name)
+  }
+}
 
-  $.ajax({
-    method: "GET",
-    url: "/tweets/search/" + keyword,
-    dataType: "json"
-  })
-  .done(function(response){
-    console.log(response.status)
-    for(var i = 0; i < response.length; i++){
-     var j = (i + 1)
-      var createdAt = (new Date(response[i].created_at))
-      var currentTime = Math.floor((Date.now() - (createdAt)) / 1000 / 60)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(currentTime + " min")
-
-      $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
-      }
-    })
-    .fail(function(response){
-      console.log("FAIL")
-      $("#search").css("background-color", "red")
-    })
-  })
-
-
-  $("#trends-container").on("click", "li", function(e){
+var searchKeypressHandler = function(e){
     e.preventDefault()
-    var hashTag = $(this).text()
-    console.log(hashTag)
+    console.log("hello??")
+    var keyword = $("#search").val()
+    console.log(keyword)
 
     $.ajax({
       method: "GET",
-      url: "/tweets/search/" + hashTag,
-      data: { name: hashTag},
+      url: "/tweets/search/" + keyword,
       dataType: "json"
     })
+    .done(function(response){ riverUpdater(response) })
+    .fail(function(response){ searchFailHandler() })
+}
 
-    .done(function(response){
-      for(var i = 0; i < response.length; i++){
-      var j = (i + 1)
-      var createdAt = (new Date(response[i].created_at))
-      var currentTime = Math.floor((Date.now() - (createdAt)) / 1000 / 60)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".full-name").text(response[i].username)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".username").text(response[i].handle)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find(".timestamp").text(currentTime + " min")
+var searchFailHandler = function() {
+    console.log("FAIL")
+    $("#search").css("background-color", "red")
+}
 
-      $("#tweets-container").find("li:nth-child(" + j + ")").find("img").attr("src", response[i].avatar_url)
-      $("#tweets-container").find("li:nth-child(" + j + ")").find("p:nth-child(2)").text(response[i].content)
-      }
-    })
+var loadPopularTweets = function() {
+  var ajaxOptions = {
+    method: "GET",
+    url: "/hashtags/popular",
+    dataType: "json"
+  }
+  $.ajax(ajaxOptions)
+    .done(popularHashtagsLoadHandler)
+}
 
+var trendsContainerClickHandler = function(e){
+  e.preventDefault()
+  var hashTag = $(this).text()
+  console.log(hashTag)
+
+  $.ajax({
+    method: "GET",
+    url: "/tweets/search/" + hashTag,
+    data: { name: hashTag},
+    dataType: "json"
   })
+  .done(function(response){ riverUpdater(response) })
+}
 
+$(document).ready(function(){
+  loadPopularTweets()
+  $("#search-form").on("submit", searchKeypressHandler)
+  $("#trends-container").on("click", "li", trendsContainerClickHandler)
 })
-
