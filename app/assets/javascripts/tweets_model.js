107a338cc88e6db78e094e02291c3c561076f355
Josh Lim & Paul Newsam & Ryan Austin <pair+joshhlim.monkypuzzle.ryana760@devbootcamp.com>
2017-06-07 10:12:08 -0700

    Initial commit

---
+++ app/assets/javascripts/tweets_model.js
@@ -0,0 +1,10 @@
+var Tweet = function (args) {
+  this.content = args.content
+  this.username = args.username
+  this.handle = args.handle
+  this.avatar_url = args.avatar_url
+  this.created_at = args.created_at
+  this.updated_at = args.updated_at
+  this.hashtag_names = args.hashtag_names
+}
+
